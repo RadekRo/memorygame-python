e3d5724cc74cfbd55575c92a6bce6b80b0a9c1aa
RadekRo <r.roclawski@ankara.pl>
2023-02-21 20:09:04 +0100

    Written display board function - tested

--- board.py
+++ board.py
@@ -14,3 +14,22 @@ def get_empty_board(width, height):
   for i in range(0, height):
     board.append(cols)
   return board
+
+def display_board(board):
+  board_width = len(board[0])
+  current_row = 1
+  draw_board = "   |"
+  for i in range(0, board_width):
+    draw_board += f" {string.ascii_uppercase[i]} |"
+  draw_board += get_new_line()
+  for row in board:
+    draw_board += draw_horizontal_line(board_width + 1)
+    draw_board += f"\n {current_row} |"
+    for col in row:
+      draw_board += f" {col} |"
+    draw_board += get_new_line()
+    current_row += 1
+  draw_board += draw_horizontal_line(board_width + 1)
+  return draw_board
+  
+print(display_board((["#", "S", "#"], ["#", "#", "#"], ["#", "#", "#"])))
