65051be8cf927ed8ddc9f5320edae33c0cec7d97
RadekRo <r.roclawski@ankara.pl>
2023-02-21 21:00:59 +0100

    Started to work on menu

--- menu.py
+++ menu.py
@@ -1,1 +1,22 @@
 from common import clear
+
+def menu():
+    clear()
+    print("""#     # ####### #     # ####### ######  #     # 
+##   ## #       ##   ## #     # #     #  #   #  
+# # # # #       # # # # #     # #     #   # #   
+#  #  # #####   #  #  # #     # ######     #    
+#     # #       #     # #     # #   #      #    
+#     # #       #     # #     # #    #     #    
+#     # ####### #     # ####### #     #    #         
+==============================================
+Developed by RadekRo. Version 1.23. 
+==============================================
+Choose a difficulty level:
+1. EASY
+2. MEDIUM
+3. HARD
+4. QUIT""")
+    choice = input("Your choice?: ")
+
+menu()
