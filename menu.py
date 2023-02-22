6e54e6c88428ac92aaa0180e412f25279b0135e7
RadekRo <r.roclawski@ankara.pl>
2023-02-22 19:41:48 +0100

    Added error message if user enters wrong menu number. Processed quit option

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ def get_user_choice():
 
 def menu():
     valid_user_entry = False
+    error_message = False
     while valid_user_entry == False:
         clear()
         print("""#     # ####### #     # ####### ######  #     # 
@@ -23,7 +24,11 @@ Choose a difficulty level:
 2. MEDIUM
 3. HARD
 4. QUIT""")
+        print("WRONG ENTRY *** ENTER VALID NUMBER (1-4)!") if error_message == True else None
         user_entry = get_user_choice()
         valid_user_entry = True if user_entry > 0 and user_entry < 5 else False
+        error_message = True
+        if user_entry == 4:
+            print("You've chosen to QUIT the game. Bye, bye!")
+            break
     return user_entry
-print(menu())
